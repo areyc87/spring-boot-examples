16747da0feb7f8ba8bd17088ebc8721241c0292d
ANDRES F REY C <arey@192.168.10.11>
2024-09-14 18:37:26 -0500

    fix test

--- src/test/java/co/edu/unbosque/programacion2/examen/ExamenProgramacion2Test.java
+++ src/test/java/co/edu/unbosque/programacion2/examen/ExamenProgramacion2Test.java
@@ -1,10 +1,10 @@
-package co.edu.unbosque.springbootdemo;
+package co.edu.unbosque.programacion2.examen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootDemoApplicationTests {
+class ExamenProgramacion2Test {
 
 	@Test
 	void contextLoads() {
